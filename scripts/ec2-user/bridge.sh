e2367314895cfbe00ebb168298cc021e304e7a6f
seansnake93 <seansnake93@hotmail.co.uk>
2021-03-05 18:53:37 +0000

    update

--- scripts/ec2-user/bridge.sh
+++ scripts/ec2-user/bridge.sh
@@ -2,10 +2,10 @@
 sudo su ubuntu
 sudo apt-get update -y
 sudo apt install awscli -y
-apt install python3 -y
-apt install python3-pip -y
-apt install python3-venv -y
-apt-get install python3-pip -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
+sudo apt-get install python3-pip -y
 apt update -y
 git config --global credential.helper cache
 git config --global user.email "${git_email}"
@@ -15,12 +15,12 @@ git clone https://github.com/${git_user}/${project_name}
 cd ./${project_name}
 chmod +x ./scripts/git/*
 chmod +x ./scripts/install/*
-sh scripts/install/terra.sh
-sh scripts/install/docker.sh
-sh scripts/install/compose.sh
-sh scripts/install/ansible.sh
-sh scripts/install/jenkins.sh
-echo ".gitignore/n" >> .gitignore
+sh ./scripts/install/terra.sh
+sh ./scripts/install/docker.sh
+sh ./scripts/install/compose.sh
+sh ./scripts/install/ansible.sh
+sh ./scripts/install/jenkins.sh
+echo ".gitignore\n" >> .gitignore
 python3 -m venv ${project_name}-venv
 echo "/${project_name}-venv/" >> .gitignore
 . ${project_name}-venv/bin/activate
